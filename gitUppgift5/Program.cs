dcf7f0495673032594fbfe26c134c961637edc3c
Pontus Flodström <pontaflod95@hotmail.com>
2024-09-27 13:32:20 +0200

    Added a new class 'myNumbers' to store objects with numbers from user input. Also added a List to store the myNumbers objects for easy access in the operation switch.

--- gitUppgift5/Program.cs
+++ gitUppgift5/Program.cs
@@ -2,32 +2,50 @@
 
 namespace EnkelKalkylator
 {
+    // Creating class myNumbers to keep track of my numbers using objects.
+    class myNumbers
+    {
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+
+        // Adding a constructor
+        public myNumbers(double num1, double num2)
+        {
+            Num1 = num1;
+            Num2 = num2;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            // I Create a list for my objects containing numbers based on user input.
+            List<myNumbers> numbersList = new List<myNumbers>();
+
+            // Take in user input
             Console.WriteLine("Enkel Kalkylator");
             Console.WriteLine("Ange operation (+, -, *, /): ");
             char operation = Console.ReadKey().KeyChar;
             Console.WriteLine("\nAnge första talet: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
+            double input_num1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Ange andra talet: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
+            double input_num2 = Convert.ToDouble(Console.ReadLine());
+            numbersList.Add(new myNumbers(input_num1, input_num2)); // I add my numbers to my List as an object.
 
             double result = 0;
             switch (operation)
             {
                 case '+':
-                    result = num1 + num2;
+                    result = numbersList[0].Num1 + numbersList[0].Num2;
                     break;
                 case '-':
-                    result = num1 - num2;
+                    result = numbersList[0].Num1 - numbersList[0].Num2;
                     break;
                 case '*':
-                    result = num1 * num2;
+                    result = numbersList[0].Num1 * numbersList[0].Num2;
                     break;
                 case '/':
-                    result = num1 / num2;
+                    result = numbersList[0].Num1 / numbersList[0].Num2;
                     break;
                 default:
                     Console.WriteLine("Ogiltig operation.");
