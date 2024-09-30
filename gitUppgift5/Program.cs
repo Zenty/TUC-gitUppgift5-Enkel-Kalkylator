7fb8ea9e4fa198dea319d3a52e174c17a083bbc7
Pontus <pontaflod95@hotmail.com>
2024-09-30 13:31:58 +0200

    I added a new 'myCalculations' method; 'Squareroot', for calculating the squareroot of a number.
    I slightly updated how the result output is working and being displayed to the end user by adding a result string inside each switch case for more flexible messages.

--- gitUppgift5/Program.cs
+++ gitUppgift5/Program.cs
@@ -9,12 +9,18 @@ public class onStartUp
     public myUserInput initiate()
     {
         Console.WriteLine("Enkel Kalkylator");
-        Console.WriteLine("Ange operation (+, -, *, /): ");
+        Console.WriteLine("Ange operation (+, -, *, /, s): "); // I'm using 'S' instead of '^' since ReadKey().KeyChar gets set with the wrong char.
         char input_operation = Console.ReadKey().KeyChar;
-        Console.WriteLine("\nAnge första talet: ");
+        Console.WriteLine("\nAnge ditt tal: ");
         double input_num1 = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Ange andra talet: ");
-        double input_num2 = Convert.ToDouble(Console.ReadLine());
+        double input_num2 = 0;
+
+        // We only need to work with one number if we're calculating the square root of a number.
+        if (input_operation != 's')
+        {
+            Console.WriteLine("Ange ditt andra tal: ");
+            input_num2 = Convert.ToDouble(Console.ReadLine());
+        }
 
         // I create a new instance of myUserInput and return the object containing all input data.
         return new myUserInput(input_operation, input_num1, input_num2);
@@ -56,6 +62,11 @@ public class myCalculations
     {
         return num1 / num2;
     }
+
+    public double Squareroot(double num1) // My method for calculating the squareroot
+    {
+        return Math.Sqrt(num1);
+    }
 }
 class Program
 {
@@ -68,27 +79,36 @@ class Program
         onStartUp onStartUp = new onStartUp();
         userInput.Add(onStartUp.initiate()); // I run the .initiate() method that takes all user input and return a new myUserInput object that I add to my list.
 
-        double result = 0;
+        double calcResult = 0;
+        string resultString = "";
         switch (userInput[0].Operation)
         {
             case '+':
-                result = new myCalculations().Addition(userInput[0].Num1, userInput[0].Num2); // I Send the numbers stored in my List as data to my calculation.
+                calcResult = new myCalculations().Addition(userInput[0].Num1, userInput[0].Num2); // I Send the numbers stored in my List as data to my calculation.
+                resultString = $"Result: {userInput[0].Num1} {userInput[0].Operation} {userInput[0].Num2} = {calcResult}";
                 break;
             case '-':
-                result = new myCalculations().Substraction(userInput[0].Num1, userInput[0].Num2); // I Send the numbers stored in my List as data to my calculation.
+                calcResult = new myCalculations().Substraction(userInput[0].Num1, userInput[0].Num2); // I Send the numbers stored in my List as data to my calculation.
+                resultString = $"Result: {userInput[0].Num1} {userInput[0].Operation} {userInput[0].Num2} = {calcResult}";
                 break;
             case '*':
-                result = new myCalculations().Multiplication(userInput[0].Num1, userInput[0].Num2); // I Send the numbers stored in my List as data to my calculation.
+                calcResult = new myCalculations().Multiplication(userInput[0].Num1, userInput[0].Num2); // I Send the numbers stored in my List as data to my calculation.
+                resultString = $"Result: {userInput[0].Num1} {userInput[0].Operation} {userInput[0].Num2} = {calcResult}";
                 break;
             case '/':
-                result = new myCalculations().Division(userInput[0].Num1, userInput[0].Num2); // I Send the numbers stored in my List as data to my calculation.
+                calcResult = new myCalculations().Division(userInput[0].Num1, userInput[0].Num2); // I Send the numbers stored in my List as data to my calculation.
+                resultString = $"Result: {userInput[0].Num1} {userInput[0].Operation} {userInput[0].Num2} = {calcResult}";
+                break;
+            case 's':
+                calcResult = new myCalculations().Squareroot(userInput[0].Num1); // I send the number stored in my List as data to my calculation.
+                resultString = $"The Square root of {userInput[0].Num1} is: {calcResult}";
                 break;
             default:
                 Console.WriteLine("Ogiltig operation.");
                 return;
         }
 
-        Console.WriteLine($"Resultat: {result}"); // Write out the result
+        Console.WriteLine(resultString); // Write out the result
         Console.ReadKey();
     }
 }
