70cc1dec86a45e64760eb428de0373a80c8938aa
Pontus <pontaflod95@hotmail.com>
2024-09-30 12:53:19 +0200

    I changed the class 'myNumbers' into 'myUserInput' for better clarity of its functions and gave it the additional variable 'Operation'.
    I also created a new class 'onStartUp' to handle all user input and the initiation of our 'myUserInput' object.
    
    The Main() method of our class Program has been cleaned up from all previous codes that's now placed into our 'OnStartUp' class.

--- gitUppgift5/Program.cs
+++ gitUppgift5/Program.cs
@@ -3,18 +3,39 @@ using System.Numerics;
 
 namespace EnkelKalkylator;
 
-// Creating class myNumbers to keep track of my numbers using objects.
-public class myNumbers
+// Creating class onStartUp to display & take user input.
+public class onStartUp
 {
+    public myUserInput initiate()
+    {
+        Console.WriteLine("Enkel Kalkylator");
+        Console.WriteLine("Ange operation (+, -, *, /): ");
+        char input_operation = Console.ReadKey().KeyChar;
+        Console.WriteLine("\nAnge första talet: ");
+        double input_num1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Ange andra talet: ");
+        double input_num2 = Convert.ToDouble(Console.ReadLine());
+
+        // I create a new instance of myUserInput and return the object containing all input data.
+        return new myUserInput(input_operation, input_num1, input_num2);
+    }
+}
+
+// Creating class myUserInput to keep track of my numbers using objects.
+public class myUserInput
+{
+    public char Operation { get; set; }
     public double Num1 { get; set; }
     public double Num2 { get; set; }
 
-    public myNumbers(double num1, double num2)
+    public myUserInput(char operation, double num1, double num2)
     {
+        Operation = operation;
         Num1 = num1;
         Num2 = num2;
     }
 }
+
 // Creating class myCalculations to create reusable methods for addition, substraction, multiplication & division.
 public class myCalculations
 {
@@ -40,41 +61,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        // I Create a list for my objects containing numbers based on user input.
-        List<myNumbers> numbersList = new List<myNumbers>();
+        // I Create a list for my objects containing user input.
+        List<myUserInput> userInput = new List<myUserInput>();
 
-
-        // Take in user input
-        Console.WriteLine("Enkel Kalkylator");
-        Console.WriteLine("Ange operation (+, -, *, /): ");
-        char operation = Console.ReadKey().KeyChar;
-        Console.WriteLine("\nAnge första talet: ");
-        double input_num1 = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Ange andra talet: ");
-        double input_num2 = Convert.ToDouble(Console.ReadLine());
-        numbersList.Add(new myNumbers(input_num1, input_num2)); // I add my numbers to my List as an object.
+        // I Create a new instance of my onStartUp class
+        onStartUp onStartUp = new onStartUp();
+        userInput.Add(onStartUp.initiate()); // I run the .initiate() method that takes all user input and return a new myUserInput object that I add to my list.
 
         double result = 0;
-        switch (operation)
+        switch (userInput[0].Operation)
         {
             case '+':
-                result = new myCalculations().Addition(numbersList[0].Num1, numbersList[0].Num2); // I Send the numbers stored in my List as data to my calculation.
+                result = new myCalculations().Addition(userInput[0].Num1, userInput[0].Num2); // I Send the numbers stored in my List as data to my calculation.
                 break;
             case '-':
-                result = new myCalculations().Substraction(numbersList[0].Num1, numbersList[0].Num2); // I Send the numbers stored in my List as data to my calculation.
+                result = new myCalculations().Substraction(userInput[0].Num1, userInput[0].Num2); // I Send the numbers stored in my List as data to my calculation.
                 break;
             case '*':
-                result = new myCalculations().Multiplication(numbersList[0].Num1, numbersList[0].Num2); // I Send the numbers stored in my List as data to my calculation.
+                result = new myCalculations().Multiplication(userInput[0].Num1, userInput[0].Num2); // I Send the numbers stored in my List as data to my calculation.
                 break;
             case '/':
-                result = new myCalculations().Division(numbersList[0].Num1, numbersList[0].Num2); // I Send the numbers stored in my List as data to my calculation.
+                result = new myCalculations().Division(userInput[0].Num1, userInput[0].Num2); // I Send the numbers stored in my List as data to my calculation.
                 break;
             default:
                 Console.WriteLine("Ogiltig operation.");
                 return;
         }
 
-        Console.WriteLine($"Resultat: {result}");
+        Console.WriteLine($"Resultat: {result}"); // Write out the result
         Console.ReadKey();
     }
 }
